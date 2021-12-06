5cfa12493ebf18797a52b5a94734f2f89415bb04
yair <aythb@hotmail.com>
2021-12-06 17:16:06 -0600

    codigo de prueba

---
+++ readCreateImage.py
@@ -0,0 +1,10 @@
+import cv2
+
+path = r'C:\Users\OptiPlex\Desktop\TODO YAIR\PYTHON 3 openCV\readCreateImage\im.jpg'
+imagen = cv2.imread(path, 0)
+window_name = 'Lectura de imagen'
+nombre = "imagengris.jpg"
+cv2.imshow(window_name,imagen)
+cv2.imwrite("nombreimagengris.jpg", imagen)
+cv2.waitKey(2000)
+cv2.destroyAllWindows()
